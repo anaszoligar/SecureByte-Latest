f8ce8d64812ff9fc0c5ea3964b28ebff701e2e17
user76 <30927722+user76-OFFICIAL@users.noreply.github.com>
2024-05-01 19:55:38 +0300

    Add files via upload

---
+++ VMUtils/Utils.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+namespace VMUtils
+{
+    public class Utils
+    {
+        public static HashSet<string> hashSet = new HashSet<string>();
+        public static List<string> tempMethodsList = new List<string>();
+        public static bool ProtectAll = false;
+        public static string SelectedMethods;
+        public static string password = "";
+    }
+}
